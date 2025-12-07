fb6b22b894e96bf20e71d9b2096985a9d4248a95
Jiyboo <afsalonline01@gmail.com>
2025-12-07 16:18:11 +0700

    Update database

--- db-parking/parkir.sql
+++ db-parking/parkir.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Des 2025 pada 15.03
+-- Waktu pembuatan: 07 Des 2025 pada 10.17
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
